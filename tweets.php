491e0c7a0d5436bf752e1e887d7eaf0d48ead530
Ben Everard <hello@beneverard.co.uk>
2016-02-09 13:32:16 +0000

    Added settings file suport
    Settings can be saved without appearing in the git repo

--- tweets.php
+++ tweets.php
@@ -15,7 +15,7 @@
 	 * times an hour per user client.
 	 * --
 	 * Dates can be displayed in Twitter style (e.g. "1 hour ago") by setting the 
-	 * $twitter_style_dates param to true.
+	 * $settings->twitter_style_dates param to true.
 	 *
 	 * You will also need to register your application with Twitter, to get your keys and tokens.
 	 * You can do this here: (https://dev.twitter.com/).
@@ -48,7 +48,33 @@
 	
 	// Set timezone. (Modify to match your timezone) If you need help with this, you can find it here. (http://php.net/manual/en/timezones.php)
 	date_default_timezone_set('Europe/London');
-	
+
+	function get_settings() {
+
+		$settings_json = array();
+
+		if ( file_exists('settings.json') ) {
+			$settings_json = json_decode(file_get_contents('settings.json'), TRUE);
+		}
+
+		// print_r($settings_json);
+
+		return (object) array_merge([
+			'consumer_key' => '',
+			'consumer_secret' => '',
+			'access_token' => '',
+			'access_token_secret' => '',
+			'cache_file' => './tweets.json',
+			'tweets_to_display' => 20,
+			'ignore_replies' => TRUE,
+			'include_rts' => FALSE,
+			'date_format' => 'g:i A M jS',
+			'twitter_style_dates' => TRUE,
+			'cache_time' => 60 * 3
+		], $settings_json);
+
+	}
+
 	// Require TwitterOAuth files. (Downloadable from : https://github.com/abraham/twitteroauth)
 	require_once("twitteroauth/twitteroauth/twitteroauth.php");
 	
@@ -59,49 +85,33 @@
 	}
 	
 	// Function to display the latest tweets.
-	function display_latest_tweets(
-		
-		// Function parameters.
-		$twitter_user_id,
-		$cache_file          = './tweets.json',  // Change this to the path of your cache file. (Default : ./tweets.txt)
-		$tweets_to_display   = 20,               // Number of tweets you would like to display. (Default : 5)
-		$ignore_replies      = true,           // Ignore replies from the timeline. (Default : false)
-		$include_rts         = false,           // Include retweets. (Default : false)
-		$date_format         = 'g:i A M jS',    // Date formatting. (http://php.net/manual/en/function.date.php)
-		$twitter_style_dates = true){           // Twitter style days. [about an hour ago] (Default : true)
-			
-		// Twitter keys (You'll need to visit https://dev.twitter.com and register to get these.
-		$consumerkey         = getenv('CONSUMERKEY');
-		$consumersecret      = getenv('CONSUMERSECRET');
-		$accesstoken         = getenv('ACCESSTOKEN');
-		$accesstokensecret   = getenv('ACCESSTOKENSECRET');
-		
-		// Seconds to cache feed (Default : 3 minutes).
-		$cachetime           = 60*3;
-		
+	function display_latest_tweets($twitter_user_id) {
+
+		$settings = get_settings();
+
 		// Time that the cache was last updtaed.
-		$cache_file_created  = ((file_exists($cache_file))) ? filemtime($cache_file) : 0;
+		$cache_file_created  = ((file_exists($settings->cache_file))) ? filemtime($settings->cache_file) : 0;
  
 		// A flag so we know if the feed was successfully parsed.
 		$tweet_found         = false;
 		
-		// Show cached version of tweets, if it's less than $cachetime.
-		if ( time() - $cachetime < $cache_file_created ) {
+		// Show cached version of tweets, if it's less than $settings->cache_time.
+		if ( time() - $settings->cache_time < $cache_file_created ) {
 
 			$tweet_found = true;
 
 			// Display tweets from the cache.
-			readfile($cache_file);
+			readfile($settings->cache_file);
 
 		} else {
 		
 			// Cache file not found, or old. Authenticae app.
-			$connection = getConnectionWithAccessToken($consumerkey, $consumersecret, $accesstoken, $accesstokensecret);
+			$connection = getConnectionWithAccessToken($settings->consumer_key, $settings->consumer_secret, $settings->access_token, $settings->access_token_secret);
 			
 			if ( $connection ) {
 
 				// Get the latest tweets from Twitter
-				$get_tweets = $connection->get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=".$twitter_user_id."&count=".$tweets_to_display."&include_rts=".$include_rts."&exclude_replies=".$ignore_replies);
+				$get_tweets = $connection->get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=".$twitter_user_id."&count=".$settings->tweets_to_display."&include_rts=".$settings->include_rts."&exclude_replies=".$settings->ignore_replies);
 				
 				// Error check: Make sure there is at least one item.
 				if (count($get_tweets)) {
@@ -145,7 +155,7 @@
 						// Convert Tweet display time to a UNIX timestamp. Twitter timestamps are in UTC/GMT time.
 						$tweet_time = strtotime($tweet->created_at);
 
-						if ( $twitter_style_dates ) {
+						if ( $settings->twitter_style_dates ) {
 
 							// Current UNIX timestamp.
 							$current_time = time();
@@ -174,19 +184,19 @@
 									break;
 
 								default:
-									$display_time = date($date_format,$tweet_time);
+									$display_time = date($settings->date_format, $tweet_time);
 									break;
 
 							}
 
 						} else {
-							$display_time = date($date_format,$tweet_time);
+							$display_time = date($settings->date_format, $tweet_time);
 						}
 
 						$tweets[$tweet->id]['display_time'] = $display_time;
 
 						// If we have processed enough tweets, stop.
-						if ($tweet_count >= $tweets_to_display){
+						if ($tweet_count >= $settings->tweets_to_display){
 							break;
 						}
  
@@ -200,7 +210,7 @@
 					echo json_encode($output);
  
 					// Generate a new cache file.
-					$file = fopen($cache_file, 'w');
+					$file = fopen($settings->cache_file, 'w');
  
 					// Save the contents of output buffer to the file, and flush the buffer. 
 					fwrite($file, json_encode($output)); 
